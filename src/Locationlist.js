10cb1923808506c57504f3b57dbcef3509565655
mcboxez <32357520+mcboxez@users.noreply.github.com>
2018-08-19 13:52:59 +0800

    Add files via upload

---
+++ src/Locationlist.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import locations from'./locations';
+
+
+export class Locationlist extends React.Component {
+
+
+
+    componentDidMount(){
+      this.setState({
+      	locations: locations
+      })
+    }
+
+
+//读取App.js的onPlaceClick数据
+    handleClick = (place) => {
+      this.props.onPlaceClick(place);
+    };
+
+  //更新搜索框数据，筛选出搜索框与地点列表的相同项  
+    updateQuery = (query) => {
+      this.props.updatePlaces(query);
+  }
+
+
+   	render(){
+		return(
+		  <div>
+		    <p>Location List</p>
+		    <input type="text" aria-label="input" tabIndex="1" onChange= { event => this.updateQuery(event.target.value)}  />
+		    <ul>
+		    {this.props.locations.map(location => (
+              <li
+                key={location.title}
+                onClick={(place) => this.handleClick(place)}
+                tabIndex="2"
+                role="button"
+              >
+                {location.title}
+              </li>
+            ))}
+		    </ul>
+		    
+		  </div>
+		)
+
+
+		
+        
+		  
+	   	
+	   }
+	
+
+
+}
+
+
+export default Locationlist
